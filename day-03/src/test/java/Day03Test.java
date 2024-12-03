6b7631e764b94b214f05be261ce3b224abefe9bf
luschmar <90399580+luschmar@users.noreply.github.com>
2024-12-03 08:19:17 +0100

    🧑‍💻 solve day-03 puzzle; part 2

--- day-03/src/test/java/Day03Test.java
+++ day-03/src/test/java/Day03Test.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.params.ParameterizedTest;
 
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
@@ -21,11 +22,31 @@ class Day03Test {
 
 	@ParameterizedTest
 	@AocFileSource(inputs = {
-			@AocInputMapping(input = "test.txt", expected = ""),
-			@AocInputMapping(input = "input.txt", expected = "")
+			@AocInputMapping(input = "test2.txt", expected = "48"),
+			@AocInputMapping(input = "input.txt", expected = "95411583")
 	})
 	void part2(Stream<String> input, String expected) {
-		String res = "res";
-		assertEquals(expected,res);
+		var mul = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)|don't\\(\\)|do\\(\\)");
+
+		var on = new AtomicBoolean(true);
+		var res = input.flatMap(s -> mul.matcher(s).results()).mapToLong(a ->
+				{
+					var t = a.group(0);
+					if (t.startsWith("do(")) {
+						on.set(true);
+						return 0;
+					}
+					if (t.startsWith("don't")) {
+						on.set(false);
+						return 0;
+					}
+					if (on.get()) {
+						return Math.multiplyExact(Long.parseLong(a.group(1)), Long.parseLong(a.group(2)));
+					}
+					return 0;
+				}
+		).sum();
+
+		assertEquals(Long.parseLong(expected), res);
 	}
 }
