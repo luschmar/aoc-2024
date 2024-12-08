3f095dc285ed5e73bf06431741e26a1bd2032c4b
luschmar <90399580+luschmar@users.noreply.github.com>
2024-12-08 11:42:55 +0100

    🧑‍💻 solve day-08 part1 and part2

--- day-08/src/test/java/Day08Test.java
+++ day-08/src/test/java/Day08Test.java
@@ -1,86 +1,127 @@
 import org.junit.jupiter.params.ParameterizedTest;
 
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day08Test {
-	@ParameterizedTest
-	@AocFileSource(inputs = {
-			@AocInputMapping(input = "test.txt", expected = "14"),
-			@AocInputMapping(input = "test1.txt", expected = "2"),
-			@AocInputMapping(input = "test2.txt", expected = "2"),
-			@AocInputMapping(input = "https://adventofcode.com/2024/day/8/input", expected = "-1")
-	})
-	void part1(Stream<String> input, String expected) {
-		var map = input.toList();
-		var antennas = IntStream.range(0, map.size()).mapToObj(y ->  IntStream.range(0, map.get(y).length()).mapToObj(
-				x -> {
-					var c = map.get(y).charAt(x);
-					if(c != '.') {
-						return new AntennaLocation(x, y, c);
-					}
-					return null;
-				}
-		)).flatMap(s -> s).filter(Objects::nonNull).toList();
+    @ParameterizedTest
+    @AocFileSource(inputs = {
+            @AocInputMapping(input = "test.txt", expected = "14"),
+            @AocInputMapping(input = "test1.txt", expected = "2"),
+            @AocInputMapping(input = "test2.txt", expected = "4"),
+            @AocInputMapping(input = "test3.txt", expected = "4"),
+            @AocInputMapping(input = "https://adventofcode.com/2024/day/8/input", expected = "398")
+    })
+    void part1(Stream<String> input, String expected) {
+        var map = input.toList();
+        var antennas = IntStream.range(0, map.size()).mapToObj(y -> IntStream.range(0, map.get(y).length()).mapToObj(
+                x -> {
+                    var c = map.get(y).charAt(x);
+                    if (c != '.' && c != '#') {
+                        return new AntennaLocation(x, y, c);
+                    }
+                    return null;
+                }
+        )).flatMap(s -> s).filter(Objects::nonNull).toList();
 
-		long antinodeCount = 0;
-		for(var a : antennas){
-			var otherAntennas = antennas.stream().filter(o -> o != a).toList();
-			antinodeCount += otherAntennas.stream()
-					.filter(s -> a.frequency == s.frequency)
-					.map(b -> new AntennaPair(a, b))
-					.filter(c -> c.countAntinodes(otherAntennas))
-					.count();
-		}
+        Set<AntinodeLocation> antinodes = new HashSet<>();
+        for (var a : antennas) {
+            var otherAntennas = antennas.stream().filter(o -> o != a).toList();
 
-		assertEquals(Integer.parseInt(expected), antinodeCount);
-	}
+            var nodes = otherAntennas.stream()
+                    .filter(s -> a.frequency == s.frequency)
+                    .map(b -> new AntennaPair(a, b))
+                    .map(c -> c.toAntiNodeInMap(map.getFirst().length(), map.size())).filter(Objects::nonNull).collect(Collectors.toSet());
+            antinodes.addAll(nodes);
+        }
 
-	@ParameterizedTest
-	@AocFileSource(inputs = {
-			@AocInputMapping(input = "test.txt", expected = ""),
-			//@AocInputMapping(input = "https://adventofcode.com/2024/day/8/input", expected = "-1")
-	})
-	void part2(Stream<String> input, String expected) {
-		String res = "res";
-		assertEquals(expected,res);
-	}
+        assertEquals(Integer.parseInt(expected), antinodes.size());
+    }
 
-	record AntennaLocation(int x, int y, char frequency) {
+    @ParameterizedTest
+    @AocFileSource(inputs = {
+            @AocInputMapping(input = "test.txt", expected = "34"),
+            @AocInputMapping(input = "test4.txt", expected = "9"),
+            @AocInputMapping(input = "https://adventofcode.com/2024/day/8/input", expected = "1333")
+    })
+    void part2(Stream<String> input, String expected) {
+        var map = input.toList();
+        var antennas = IntStream.range(0, map.size()).mapToObj(y -> IntStream.range(0, map.get(y).length()).mapToObj(
+                x -> {
+                    var c = map.get(y).charAt(x);
+                    if (c != '.' && c != '#') {
+                        return new AntennaLocation(x, y, c);
+                    }
+                    return null;
+                }
+        )).flatMap(s -> s).filter(Objects::nonNull).toList();
 
-	}
+        Set<AntinodeLocation> antinodes = new HashSet<>();
+        for (var a : antennas) {
+            var otherAntennas = antennas.stream().filter(o -> o != a).toList();
 
-	record AntennaPair(AntennaLocation l1, AntennaLocation l2) {
-		boolean countAntinodes(List<AntennaLocation> otherAntennas) {
-			var aX =  l1.x + (l1.x - l2.x);
-			var aY = l1.y + (l1.y - l2.y);
+            var nodes = otherAntennas.stream()
+                    .filter(s -> a.frequency == s.frequency)
+                    .map(b -> new AntennaPair(a, b))
+                    .flatMap(c -> c.toAntiNodesInMap(map.getFirst().length(), map.size()).stream())
+                    .filter(Objects::nonNull).collect(Collectors.toSet());
+            antinodes.addAll(nodes);
+        }
 
-			var candidate = otherAntennas.stream().filter(s -> s.x == aX && s.y == aY).findFirst();
-			if(candidate.isPresent()) {
-				var cand = candidate.get();
-				var bX = l2.x + -1*(l1.x - l2.x);
-				var bY = l2.y + -1*(l1.y - l2.y);
-				var candidate2 = otherAntennas.stream().filter(s -> s.x == bX && s.y == bY).findFirst();
-				if(candidate2.isPresent()){
-					return cand.frequency == candidate2.get().frequency;
-				}
+        assertEquals(Integer.parseInt(expected), antinodes.size());
+    }
 
-			}
-			return false;
-		}
-	}
 
-	static int gcdByBruteForce(int n1, int n2) {
-		int gcd = 1;
-		for (int i = 1; i <= n1 && i <= n2; i++) {
-			if (n1 % i == 0 && n2 % i == 0) {
-				gcd = i;
-			}
-		}
-		return gcd;
-	}
+    record AntinodeLocation(int x, int y) {
+    }
+
+    record AntennaLocation(int x, int y, char frequency) {
+
+    }
+
+    record AntennaPair(AntennaLocation l1, AntennaLocation l2) {
+        AntinodeLocation toAntiNodeInMap(int maxX, int maxY) {
+            var aX = l1.x + (l1.x - l2.x);
+            var aY = l1.y + (l1.y - l2.y);
+            if ((aX >= 0 && aX < maxX) && (aY >= 0 && aY < maxY)) {
+                return new AntinodeLocation(aX, aY);
+            }
+            return null;
+        }
+
+        List<AntinodeLocation> toAntiNodesInMap(int maxX, int maxY) {
+            var sX = l1.x - l2.x;
+            var sY = l1.y - l2.y;
+            var gaX = sX / gcdByBruteForce(sX, sY);
+            var gaY = sY / gcdByBruteForce(sX, sY);
+
+            var nX = l1.x + gaX;
+            var nY = l1.y + gaY;
+
+            var result = new ArrayList<AntinodeLocation>();
+            for (int i = 1; (nX >= 0 && nX < maxX) && (nY >= 0 && nY < maxY); i++) {
+                nX = l1.x + gaX * i;
+                nY = l1.y + gaY * i;
+                if ((nX >= 0 && nX < maxX) && (nY >= 0 && nY < maxY)) {
+                    result.add(new AntinodeLocation(nX, nY));
+                }
+            }
+            result.add(new AntinodeLocation(l1.x, l1.y));
+            return result;
+        }
+    }
+
+    static int gcdByBruteForce(int n1, int n2) {
+        int gcd = 1;
+        for (int i = 1; i <= n1 && i <= n2; i++) {
+            if (n1 % i == 0 && n2 % i == 0) {
+                gcd = i;
+            }
+        }
+        return gcd;
+    }
 }
